76d23a9197f67a55e1ee338993e1b97c4435bf85
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-04-17 18:22:15 -0400

    task2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+//saving actual time to local storage
+const actualTimeDuration = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(actualTimeDuration, 900));
+
+//getting saved time from local storage and populating after user refreshed browser
+let durationTime = localStorage.getItem('videoplayer-current-time');
+player
+  .setCurrentTime(durationTime)
+  .then(() => {})
+  .catch(error => {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
