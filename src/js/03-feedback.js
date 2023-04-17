e334a9a873f1fe0cc437d8690f83d1cd6f835755
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-04-17 19:30:41 -0400

    test3 updated

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+var throttle = require('lodash.throttle');
 
+const formData = {};
+const formEl = document.querySelector('.feedback-form');
+const emailInputEl = document.querySelector("input[name='email']");
+const textInputEl = document.querySelector("textarea[name='message']");
+
+const gettingFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (gettingFormData !== null) {
+    emailInputEl.value =
+      gettingFormData.email.length > 0 ? gettingFormData.email : '';
+    textInputEl.value =
+      gettingFormData.message.length > 0 ? gettingFormData.message : '';
+    formData['email'] = emailInputEl.value;
+    formData['message'] = textInputEl.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }
+});
+const onInput = e => {
+  formData[e.target.name.trim()] = e.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  if (formData.email === undefined) {
+    formData.email = '';
+  }
+  if (formData.message === undefined) {
+    formData.message = '';
+  }
+};
+const onSubmit = () => {
+  localStorage.removeItem('feedback-form-state');
+  formEl.reset();
+};
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmit);
